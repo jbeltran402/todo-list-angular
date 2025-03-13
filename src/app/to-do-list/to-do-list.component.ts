b88b90642f29af7d20c64149561de452e34c4690
jbeltran402 <juandavid72009@gmail.com>
2025-03-12 23:18:12 -0500

    feat(components): add initial component structure

---
+++ src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ClockComponent } from '../clock/clock.component';
+
+@Component({
+  selector: 'app-to-do-list',
+  imports: [],
+  templateUrl: './to-do-list.component.html',
+  styleUrl: './to-do-list.component.css'
+})
+export class ToDoListComponent {
+  
+}
