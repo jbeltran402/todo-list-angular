e7b058dda7c129e7b41790768b5af5d1c29d1a4c
jbeltran402 <juandavid72009@gmail.com>
2025-04-19 00:54:58 -0500

    Style(Web): Implemented CSS Grid to enhance interface layout

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './clock.component.css'
 })
 export class ClockComponent {
-
+  
 }
--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 
 export class ToDoListComponent {
   newTaskDescription: string = '';
+  draggedIndex: number | null = null;
   taskList: TaskList[] = [
     {
       params: {
@@ -39,5 +40,35 @@ export class ToDoListComponent {
 
   delteTask(id:number): void{
     this.taskList.splice(id, 1);
+  }
+
+  onDragStart(event: DragEvent, index: number): void {
+    this.draggedIndex = index;
+    // event.dataTransfer?.setData('text/plain', index.toString());
+  }
+
+  onDragOver(event: DragEvent): void {
+    event.preventDefault(); // Permite que el elemento se pueda soltar aquí
+  }
+
+  onDrop(event: DragEvent, targetIndex: number): void {
+    event.preventDefault();
+    if (this.draggedIndex !== null) {
+      const draggedItem = this.taskList.splice(this.draggedIndex, 1)[0];
+      this.taskList.splice(targetIndex, 0, draggedItem);
+      this.draggedIndex = null;
+    }
+  }
+
+  addFinishedHour(index:number):void{
+    const currentDate:Date = new Date();
+    if (this.taskList[index]?.params) {
+      this.taskList[index].params.finish_time = currentDate;
+    }
+  }
+  removeFinishedHour(index:number):void{
+    if (this.taskList[index]?.params) {
+      this.taskList[index].params.finish_time = undefined;
+    }
   }
 }
