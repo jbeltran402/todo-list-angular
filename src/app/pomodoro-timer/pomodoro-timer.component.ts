3b3f8c1f9e8aea1f82bb5e85f946bc0c26688b27
jbeltran402 <juandavid72009@gmail.com>
2026-01-22 02:45:42 -0500

    feat(pomodoro): enhance timer functionality with improved time display and control buttons

--- src/app/pomodoro-timer/pomodoro-timer.component.ts
+++ src/app/pomodoro-timer/pomodoro-timer.component.ts
@@ -1,42 +1,63 @@
 import { Component, signal } from '@angular/core';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-pomodoro-timer',
+  imports: [DatePipe],
   templateUrl: './pomodoro-timer.component.html',
   styleUrl: './pomodoro-timer.component.css'
 })
 export class PomodoroTimerComponent {
 
-  default_seconds: number = 60
+  default_seconds: number = 1500
   seconds = signal(this.default_seconds);
   private timerId: any;
+
+  status: boolean = true;
+
+  private stop_and_clean_interval(){
+    if (this.timerId) {
+        clearInterval(this.timerId);
+        this.seconds.set(this.default_seconds);
+    }
+    this.status = true;
+  }
 
   countdown_timer(action: string) {
 
     switch (action) {
       case ("increase_time"):
-        this.default_seconds += 10;
-        this.seconds.update(s => s + 10);
+        // 86340s = 23h:59m:00s
+        this.default_seconds = Math.min(this.default_seconds + 60, 86340);
+        this.seconds.update(s => Math.min(s + 60, 86340));
         break;
       case ("reduce_time"):
-        this.default_seconds -= 10;
-        this.seconds.update(s => s - 10);
+        this.default_seconds = Math.max(0, this.default_seconds - 60);
+        this.seconds.update(s => Math.max(0, s - 60));
+        if (this.seconds() === 0){
+          this.stop_and_clean_interval();
+        }
         break;
       case ("start"):
         clearInterval(this.timerId);
         this.timerId = setInterval(() => {
           if (this.seconds() > 0) {
             this.seconds.update(s => s - 1);
+            this.status = false;
           } else {
-            clearInterval(this.timerId);
+            this.stop_and_clean_interval();
           }
         }, 1000); // 1000ms = 1 segundo
         break;
       case ("restar"):
-        this.seconds.set(this.default_seconds);
+        this.stop_and_clean_interval();
         break;
       case ("stop"):
         clearInterval(this.timerId);
+        this.status = true;
     }
+  }
+  ngOnDestroy() {
+    this.stop_and_clean_interval()
   }
 }
