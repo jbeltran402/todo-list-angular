da1c9379bac364344601bd7581b54239f8896243
jbeltran402 <juandavid72009@gmail.com>
2026-02-08 21:19:29 -0500

    feat(clock): implement clock component with time format toggle and real-time updates

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy, inject, PLATFORM_ID } from '@angular/core';
+import { DatePipe, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-clock',
-  imports: [],
+  standalone: true,
+  imports: [DatePipe],
   templateUrl: './clock.component.html',
   styleUrl: './clock.component.css'
 })
-export class ClockComponent {
-  
+export class ClockComponent implements OnInit, OnDestroy {
+  format = signal(false);
+  clock = signal<Date | null>(null);
+  private timerId: any;
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Captura la hora del cliente (No del servidor)
+      this.clock.set(new Date());
+      this.timerId = setInterval(() => {
+        this.clock.set(new Date());
+      }, 1000);
+    }
+  }
+
+  toggleFormat() {
+    this.format.update(currentValue => !currentValue);
+  }
+
+  ngOnDestroy() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
 }
